7b6ab2a96959962734846aee8a24e79628573c89
xhtmlboi <xhtmlboi@gmail.com>
2021-05-21 14:06:33 +0200

    Move tutorial on the root of examples

---
+++ examples/03_mixing_markdown_and_html/bin/my_site.ml
@@ -0,0 +1,28 @@
+open Wordpress
+
+let destination = "_build"
+let track_binary_update = Build.watch Sys.argv.(0)
+
+let may_process_markdown file =
+  let open Build in
+  if with_extension "md" file then snd process_markdown else arrow Fun.id
+;;
+
+let task =
+  process_files
+    [ "pages/" ]
+    (fun f -> with_extension "html" f || with_extension "md" f)
+    (fun file ->
+      let fname = basename file |> into destination in
+      let target = replace_extension fname "html" in
+      let open Build in
+      create_file
+        target
+        (track_binary_update
+        >>> read_file_with_metadata (module Metadata.Page) file
+        >>> may_process_markdown file
+        >>> apply_as_template (module Metadata.Page) "templates/layout.html"
+        >>^ Stdlib.snd))
+;;
+
+let () = execute task
--- examples/simple_blog/articles/about.md
+++
@@ -1,9 +0,0 @@
----
-title: About him
-date: 2021-03-27
-article_title: My Awesome Article about... my life
-article_description:
-    This is a simple article about me, myself, and I,
-    mainly for testing metadata injection!
----
-this page is about me. **MMMM** test
