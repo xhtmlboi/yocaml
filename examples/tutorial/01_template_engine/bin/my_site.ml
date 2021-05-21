1c96a1af93927ea78e2c578bdcfbc5ca4e443147
xhtmlboi <xhtmlboi@gmail.com>
2021-05-21 12:02:08 +0200

    Update simple template tutoriel

--- examples/tutorial/01_template_engine/bin/my_site.ml
+++ examples/tutorial/01_template_engine/bin/my_site.ml
@@ -1,16 +1,18 @@
 open Wordpress
 
 let destination = "_build"
+let track_binary_update = Build.watch Sys.argv.(0)
 
 let task =
-  process_files ["pages/"] (with_extension "html") (fun file ->
+  process_files [ "pages/" ] (with_extension "html") (fun file ->
       let target = basename file |> into destination in
       let open Build in
-      create_file target (
-        read_file "templates/header.html"
+      create_file
+        target
+        (track_binary_update
+        >>> read_file "templates/header.html"
         >>> pipe_content file
-        >>> pipe_content "templates/footer.html")
-    )
+        >>> pipe_content "templates/footer.html"))
+;;
 
-let () =
-  execute task
+let () = execute task
