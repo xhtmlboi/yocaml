dbb009938771ff29df40c012ebf6ad2760a9ab4d
gr-im <grimfw@gmail.com>
2021-07-30 01:08:13 +0200

    Small fix on Yocaml_unix documentation

--- lib/yocaml_unix/yocaml_unix.mli
+++ lib/yocaml_unix/yocaml_unix.mli
@@ -1,9 +1,9 @@
-(** {1 High-level API}
+(** A runtime describes the set of "low-level" primitives to operate in a
+    specific context. This separation allows to have a pure and platform
+    agnostic kernel (the [Yocaml] module) and to define specific runtimes as
+    needed. Here is the runtime for UNIXish (OSX/Linux). *)
 
-    The YOCaml high-level API. It is mainly these combiners that should be
-    used to build static pages.
+(** {1 API} *)
 
-    {2 Composing and performing effects} *)
-
-(** Runs a YOCaml program with the default handler. *)
+(** Executes an YOCaml program using the UNIX Runtime. *)
 val execute : 'a Yocaml.Effect.t -> 'a
