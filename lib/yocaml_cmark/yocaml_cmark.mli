6bf16b45bed2b3b0c1f3fff33ca17dae7723641f
Calascibetta Romain <romain.calascibetta@gmail.com>
2023-04-17 15:03:53 +0200

    Update the documentation

--- lib/yocaml_cmark/yocaml_cmark.mli
+++ lib/yocaml_cmark/yocaml_cmark.mli
@@ -8,9 +8,17 @@
     {{:https://github.com/dbuenzli/cmarkit} cmarkit}, an excellent Common Mark
     conversion library. *)
 
+(** CommonMark is a specification (see RFC7763/RFC7764) of Markdown since 2016.
+    As Markdown, it's a popular markup language and an implementation in OCaml
+    exists: {{:https://github.com/dbuenzli/cmarkit} cmarkit}. *)
+
 (** {1 API} *)
 
-(** An arrow that produces an HTML (as a String) from a String in Common Mark.*)
+(** An arrow that produces an HTML (as a String) from a String in Common Mark.
+
+    The [strict] argument permits to follow {b only} the CommonMark
+    specification. If you attempt to use some extensions (see
+    {!val:Cmarkit.Doc.of_string}), you should set it to [false]. *)
 val to_html : strict:bool -> (string, string) Yocaml.Build.t
 
 (** Since it is pretty common to deal with document and Metadata, which are
