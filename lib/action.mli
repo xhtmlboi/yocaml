653d8132f7a31e13353a2adb45e4478e78f7031c
xhtmlboi <xhtmlboi@gmail.com>
2024-02-28 20:45:50 +0100

    Add a dedicated type for Action
    As mentioned by @xvw, it is necessary to maintain the cache when
    producing compilation artifacts. It is therefore relevant to consider
    that a function that produces an artifact will have the normal form
    `cache -> cache fx`.
    
    Co-authored-by: xvw <xaviervdw@gmail.com>

--- lib/action.mli
+++ lib/action.mli
@@ -17,13 +17,17 @@
 (** Actions consume {!module:Pipeline} to produce artifacts. This is the entry
     point for a {b construction rule}. *)
 
-val write_file : Path.t -> (unit, string) Task.t -> Cache.t -> Cache.t Eff.t
+type t = Cache.t -> Cache.t Eff.t
+(** As it is necessary to maintain the cache during the various artifact
+    production phases, an action is a function that takes a cache and returns
+    the modified cache, wrapped in an effect. *)
+
+val write_file : Path.t -> (unit, string) Task.t -> t
 (** [write_file cache target task] Writes [target] file with content generated
     by [task] if necessary. Returns the modified cache once the action has been
     performed. *)
 
-val copy_file :
-  ?new_name:Path.fragment -> into:Path.t -> Path.t -> Cache.t -> Cache.t Eff.t
+val copy_file : ?new_name:Path.fragment -> into:Path.t -> Path.t -> t
 (** [copy_file ?new_name ~into:target cache source] Copies the [source] file to
     the [target] directory (potentially giving it a new name), taking account of
     dependencies. *)
