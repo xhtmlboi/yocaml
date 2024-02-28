f13c5577b64634a7d7c1e5c5ae70bf36e498add0
xhtmlboi <xhtmlboi@gmail.com>
2024-02-28 20:45:50 +0100

    Add a dedicated type for Action
    As mentioned by @xvw, it is necessary to maintain the cache when
    producing compilation artifacts. It is therefore relevant to consider
    that a function that produces an artifact will have the normal form
    `cache -> cache fx`.
    
    Co-authored-by: xvw <xaviervdw@gmail.com>

--- lib/action.ml
+++ lib/action.ml
@@ -14,6 +14,8 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>. *)
 
+type t = Cache.t -> Cache.t Eff.t
+
 let perform_writing cache target fc hc =
   let open Eff.Syntax in
   let* () = Lexicon.target_is_written target in
