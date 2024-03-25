1f138ead521595ea94e713827d69f198dfe46d8a
grrm <grimfw@gmail.com>
2024-03-25 01:47:24 +0100

    Simplification of file writing logic
    Now that we know statically whether an arrow produces dynamic
    dependencies or not, we can simplify the rewrite calculation.

--- lib/action.ml
+++ lib/action.ml
@@ -15,6 +15,39 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>. *)
 
 type t = Cache.t -> Cache.t Eff.t
+type interaction = Create | Nothing | Update
+
+let need_update cache has_dynamic_deps deps target =
+  let open Eff.Syntax in
+  let* exists = Eff.file_exists ~on:`Target target in
+  if exists then
+    let need_shortcut, deps =
+      if has_dynamic_deps then
+        match Cache.get cache target with
+        | None ->
+            (* If there's no information in the cache, it's annoying and dynamic
+               dependencies will probably have to be rechecked, right? In some
+               cases, a file will be rebuilt, which is a bit of a shame, but it
+               allows you to be less rigorous about cache maintenance, in the
+               event of corruption or concurrent access. So it's a loss that's
+               considered acceptable... sorry about that, but I'm not sure
+               there's a "panacea". *)
+            (true, deps)
+        | Some (_, dynamic_deps) ->
+            (* If an entry exists in the cache and the target is attached to
+               dynamic dependencies, try to rebuild the file taking into account
+               the dynamic dependencies. *)
+            (false, Deps.concat deps dynamic_deps)
+      else (false, deps)
+    in
+    if need_shortcut then Eff.return Update
+    else
+      let+ mtime_target = Eff.mtime ~on:`Target target
+      and+ mtime_deps = Deps.get_mtimes deps in
+      if List.exists (fun mtime_dep -> mtime_dep >= mtime_target) mtime_deps
+      then Update
+      else Nothing
+  else Eff.return Create
 
 let perform_writing target cache fc hc dynamic_deps =
   let open Eff.Syntax in
@@ -37,54 +70,19 @@ let perform_update target cache eff =
       perform_writing target cache fc hc dynamic_deps
 
 let write_file target task cache =
-  let rec aux dynamic_deps replay =
-    let open Eff.Syntax in
-    let deps, eff = Task.destruct task in
-    let* interaction =
-      Deps.need_update (Deps.concat deps dynamic_deps) target
-    in
-    match interaction with
-    | Deps.Nothing -> (
-        (* If there is nothing to do and that this is the first time we've
-           visited this case, inspect the cached dynamic dependencies.
-           From my understanding, this is the only "complicated" case of dynamic
-           dependencies. But it may change over time... sorry, I'm not very
-           professional. *)
-        match (Cache.get cache target, replay) with
-        | Some (_, dynamic_deps), false when not (Deps.is_empty dynamic_deps) ->
-            (* If an entry exists in the cache and the target is attached to
-               dynamic dependencies, try to rebuild the file taking into account
-               the dynamic dependencies. *)
-            let* () = Lexicon.found_dynamic_dependencies target in
-            aux dynamic_deps true
-        | None, false ->
-            (* If there's no information in the cache, it's annoying and dynamic
-               dependencies will probably have to be rechecked, right? In some
-               cases, a file will be rebuilt, which is a bit of a shame, but it
-               allows you to be less rigorous about cache maintenance, in the
-               event of corruption or concurrent access. So it's a loss that's
-               considered acceptable... sorry about that, but I'm not sure
-               there's a "panacea". *)
-            let* () = Lexicon.target_not_in_cache target in
-            perform_update target cache eff
-        | _ ->
-            (* As a generic case, it would seem that there's nothing to be
-               done. *)
-            let+ () = Lexicon.target_already_up_to_date target in
-            cache)
-    | Deps.Create ->
-        (* The file doesn't exist, so no matter what has been cached, all
-           effects must be performed. *)
-        let* () = Lexicon.target_need_to_be_built target in
-        let* fc, dynamic_deps = eff () in
-        let* hc = Eff.hash fc in
-        perform_writing target cache fc hc dynamic_deps
-    | Deps.Update ->
-        (* Whatever happens, we must perform the effect to recover the dynamic
-           dependencies. *)
-        perform_update target cache eff
-  in
-  aux Deps.empty false
+  let open Eff.Syntax in
+  let deps, eff, has_dynamic_deps = Task.destruct task in
+  let* interaction = need_update cache has_dynamic_deps deps target in
+  match interaction with
+  | Nothing ->
+      let+ () = Lexicon.target_already_up_to_date target in
+      cache
+  | Create ->
+      let* () = Lexicon.target_need_to_be_built target in
+      let* fc, dynamic_deps = eff () in
+      let* hc = Eff.hash fc in
+      perform_writing target cache fc hc dynamic_deps
+  | Update -> perform_update target cache eff
 
 let copy_file ?new_name ~into path cache =
   match Path.basename path with
@@ -92,9 +90,8 @@ let copy_file ?new_name ~into path cache =
   | Some fragment ->
       let name = Option.value ~default:fragment new_name in
       let dest = Path.(into / name) in
-      write_file dest
-        Task.(Pipeline.(read_file path >>> no_dynamic_deps ()))
-        cache
+      let arr = Task.(Pipeline.read_file path ||> no_dynamic_deps) in
+      write_file dest arr cache
 
 let batch ?only ?where path action cache =
   let open Eff in
