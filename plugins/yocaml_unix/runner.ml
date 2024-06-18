78208ebe19ad8715301b04a7f50d0448e7106f93
xhtmlboi <xhtmlboi@gmail.com>
2024-06-19 00:23:21 +0200

    Some small reorganization in Yocaml_unix runtime

--- plugins/yocaml_unix/runner.ml
+++ plugins/yocaml_unix/runner.ml
@@ -14,12 +14,4 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>. *)
 
-let setup_logger ?level () =
-  let header = Logs_fmt.pp_header in
-  let () = Fmt_tty.setup_std_outputs () in
-  let () = Logs.set_reporter Logs_fmt.(reporter ~pp_header:header ()) in
-  Logs.set_level level
-
-let run ?(level = Logs.Debug) ?custom_error_handler program =
-  let () = setup_logger ~level () in
-  Runner.run ?custom_error_handler program
+include Yocaml.Runtime.Make (Runtime)
