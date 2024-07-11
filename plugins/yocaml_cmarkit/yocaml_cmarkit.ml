825699ece6e492f6606ee5563d5f779608e2b3b4
Etienne Marais <dev@maiste.fr>
2024-07-11 22:32:41 +0200

    Translate yocaml_cmarkit to v2 API

---
+++ plugins/yocaml_cmarkit/yocaml_cmarkit.ml
@@ -0,0 +1,23 @@
+(* YOCaml a static blog generator.
+   Copyright (C) 2024 The Funkyworkers and The YOCaml's developers
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>. *)
+
+let to_html ~strict =
+  Yocaml.Task.lift (fun content ->
+      content
+      |> Cmarkit.Doc.of_string ~strict
+      |> Cmarkit_html.of_doc ~safe:false)
+
+let content_to_html ?(strict = true) () = Yocaml.Task.second (to_html ~strict)
