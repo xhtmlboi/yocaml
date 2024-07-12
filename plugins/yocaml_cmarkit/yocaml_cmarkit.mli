70bc6b56c2fe0cc7253a37d79839cf1a4620880b
Etienne Marais <dev@maiste.fr>
2024-07-12 19:38:46 +0200

    Make API more flexible in yocaml_cmarkit
    - Expose `strict` for `to_html` function.
    - Add the `safe` argument.

--- plugins/yocaml_cmarkit/yocaml_cmarkit.mli
+++ plugins/yocaml_cmarkit/yocaml_cmarkit.mli
@@ -18,31 +18,33 @@
     {i Blogosphere} (rather than describing all the formatting of document
     content in HTML, using <p>, <strong> and co).
 
-    [Common Mark] is a very popular markup language (did you get the joke,
-    up/down) and, fortunately, OCaml has several one very good libraries for
-    turning Common Mark into HTML. This library is a wrapper around
-    {{:https://github.com/dbuenzli/cmarkit} cmarkit}, an excellent Common Mark
-    conversion library. *)
-
-(** CommonMark is a specification (see RFC7763/RFC7764) of Markdown since 2016.
-    As Markdown, it's a popular markup language and an implementation in OCaml
-    exists: {{:https://github.com/dbuenzli/cmarkit} cmarkit}. *)
+    [Common Mark] is a specification (see RFC7763/RFC7764) of Markdown since
+    2016. As it is a very popular markup language, OCaml has several very good
+    libraries for turning Common Mark into HTML. This library is a wrapper
+    around {{:https://github.com/dbuenzli/cmarkit} cmarkit}, an excellent Common
+    Mark conversion library. *)
 
 (** {1 API} *)
 
-val to_html : strict:bool -> (string, string) Yocaml.Task.t
-(** [to_html ~strict] is an arrow that produces an HTML (as a String) from a
-    String in Common Mark.
+val to_html :
+  ?strict:bool -> ?safe:bool -> unit -> (string, string) Yocaml.Task.t
+(** [to_html ~strict ~safe ()] is an arrow that produces an HTML (as a String)
+    from a String in Common Mark.
 
-    The [strict] argument permits to follow {b only} the CommonMark
+    The [strict] argument permits to follow {b only} the Common Mark
     specification. If you attempt to use some extensions (see
-    {!val:Cmarkit.Doc.of_string}), you should set it to [false]. *)
+    {!val:Cmarkit.Doc.of_string}), you should set it to [false].
+
+    The [safe] argument ensures that any HTML code written in your Common Mark
+    document is escaped as an HTML comment. As most of the time, the markdown
+    provided comes from the user, this functionnality is disabled by default. If
+    you want to activate this feature, you should set it to [true]. *)
 
 val content_to_html :
-  ?strict:bool -> unit -> ('a * string, 'a * string) Yocaml.Task.t
+  ?strict:bool -> ?safe:bool -> unit -> ('a * string, 'a * string) Yocaml.Task.t
 (** Since it is pretty common to deal with document and Metadata, which are
     generally a pair of [Metadata] and [the content of the document],
     [content_to_html] is a function that produce an arrow which apply the Common
     Mark conversion on the second element (the content).
 
-    [content_to_html ()] is equivalent of [Yocaml.Build.snd to_html]. *)
+    [content_to_html ()] is equivalent of [Yocaml.Task.second to_html]. *)
