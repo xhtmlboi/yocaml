b4a6bab3b4ea008fade2ffcc96c7c686f282f2fd
xvw <xavier.vdw@gmail.com>
2025-03-17 12:19:52 +0100

    Fix `crypto-rng` initialize

--- plugins/yocaml_git/yocaml_git.ml
+++ plugins/yocaml_git/yocaml_git.ml
@@ -20,25 +20,21 @@ let run (module Source : Required.SOURCE) (module Clock : Mirage_clock.PCLOCK)
     ~context ?author ?email ?message ~remote ?level ?custom_error_handler
     program =
   let open Lwt.Syntax in
-  let () =
-    Mirage_crypto_rng_unix.initialize (module Mirage_crypto_rng.Fortuna)
-  in
+  let () = Mirage_crypto_rng_unix.use_default () in
   let* context = match context with `SSH -> Ssh.context () in
   let* store = Git_kv.connect context remote in
   let module Store = Git_kv.Make (Clock) in
   let module Store = struct
     include Store
+
     (* last_modified and change_and_push have a weird interaction;
        so we show the old last_modified *)
     let last_modified new_store key =
       let* r = last_modified store key in
       match r with
-      | Error `Not_found _ ->
-        last_modified new_store key
-      | _ ->
-        Lwt.return r
-  end
-  in
+      | Error (`Not_found _) -> last_modified new_store key
+      | _ -> Lwt.return r
+  end in
   Store.change_and_push ?author ?author_email:email ?message store (fun store ->
       let module Config = struct
         let store = store
