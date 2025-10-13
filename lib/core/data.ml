04aa79dd2135ac0040717877b37a8b1056d2257e
Linda-Njau <lindanjau21@gmail.com>
2025-10-13 14:01:02 +0300

    Define reusable signatures for from_data and to_data conversions

--- lib/core/data.ml
+++ lib/core/data.ml
@@ -410,7 +410,23 @@ module Validation = struct
     |> Result.map (fun (w, (x, y, z)) -> (w, x, y, z))
 
   let path = string $ Path.from_string
+  
+  module type S = sig
+    type a
+    val from_data : t -> a validated_value
+  end
+
+  let from (type a) (module M : S with type a = a) (data : t) : a validated_value =
+    M.from_data data
 end
+
+module type S = sig
+  type a
+  val to_data : a -> t
+end
+
+let into (type a) (module M : S with type a = a) (x : a) : t =
+  M.to_data x
 
 type 'a converter = 'a -> t
 type ('a, 'b) validator = 'a -> 'b Validation.validated_value
