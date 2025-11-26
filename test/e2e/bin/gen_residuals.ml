1aad15b4faa379a2ebfb314036c03e7e371b6e1b
xvw <xavier.vdw@gmail.com>
2025-11-26 18:52:52 +0100

    Tests for residuals

---
+++ test/e2e/bin/gen_residuals.ml
@@ -0,0 +1,17 @@
+module R = Yocaml.Runtime.Make (Runtime)
+
+let program () =
+  let on = `Target in
+  let target = Yocaml.Path.rel [ "residuals_build" ] in
+  let cache = Yocaml.Path.(target / "cache") in
+  let open Yocaml.Eff in
+  Yocaml.Action.with_cache ~on cache
+    (Yocaml.Batch.iter_files
+       (Yocaml.Path.rel [ "residuals" ])
+       (Yocaml.Action.copy_file ~into:target)
+    >=> Yocaml.Action.remove_residuals ~on ~target)
+
+let () =
+  let () = Array.iter print_endline Sys.argv in
+  let () = Yocaml_runtime.Log.setup ~level:`Debug () in
+  R.run program
